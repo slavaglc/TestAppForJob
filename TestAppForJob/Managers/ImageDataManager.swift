8e1d555d83ef0ba6a1fa361a9658932054b8cece
slavaglc <47712523+slavaglc@users.noreply.github.com>
2021-10-24 19:15:40 +0300

    test

--- TestAppForJob/Managers/ImageDataManager.swift
+++ TestAppForJob/Managers/ImageDataManager.swift
@@ -9,25 +9,7 @@ final class ImageDataManager {
     
     private init() {}
     
-//    public func fetchRandomData(completion: @escaping (_ imageProperties: ImageModel)->()) {
-//
-//        guard let url = URL(string: "https://dog.ceo/api/breeds/image/random" ) else { return }
-//        URLSession.shared.dataTask(with: url) { (data, _, _ ) in
-//
-//            guard let data = data else { return }
-//
-//            do {
-//                let decoder = JSONDecoder()
-//                let imageProperties = try decoder.decode(ImageModel.self, from: data)
-//                completion(imageProperties)
-//            } catch let error {
-//                print("Error with json serialization:", error)
-//            }
-//
-//        }.resume()
-//    }
-    
-    public func fetchRandomImageData(completion: @escaping (_ imageData: Data)->()) {
+    public func fetchRandomImageURL(completion: @escaping (_ url: String)->()) {
         guard let url = URL(string: "https://dog.ceo/api/breeds/image/random" ) else { return }
         URLSession.shared.dataTask(with: url) { [unowned self] (data, _, _) in
             
@@ -38,22 +20,20 @@ final class ImageDataManager {
                 guard let jsonDict = json as? [String: String] else { return }
                 guard let url = jsonDict["message"] else { return }
                 
-                    self.downloadImageData(url: url) { imageData in
-                        
-                            completion(imageData)
-                        
-                    }
+                    completion(url)
                 
-                
+//                    self.downloadImageData(url: url) { imageData in
+//                            completion(imageData, url)
+//                    }
                 
             } catch let error {
                 print(error)
             }
-            completion(data)
+           //completion(data, url)
         }.resume()
     }
     
-    private func downloadImageData(url: String, completion: @escaping (_ imageData: Data)->()) {
+    func downloadImageData(url: String, completion: @escaping (_ imageData: Data)->()) {
         guard let url = URL(string: url) else { return }
         URLSession.shared.dataTask(with: url) { (data, _, _) in
             guard let data = data else { return }
--- TestAppForJob/ViewControllers/ImageListViewController.swift
+++ TestAppForJob/ViewControllers/ImageListViewController.swift
@@ -7,37 +7,168 @@
 
 import UIKit
 
-final class ImageListViewController: UICollectionViewController {
-    let imageDataManager = ImageDataManager.shared
+@available(iOS 13.0, *)
+final class ImageListViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout  {
+    private let imageDataManager = ImageDataManager.shared
+    private let maxCount = 300
+    private var imageLinks = [String]()
+//    private var images: [UIImage?] = []
+    private var directionIsDown = false
+    private var contentOffset = CGPoint.zero
+    private var currentOffset = CGPoint.zero
+    private var cvCounter = 1
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        imageDataManager.fetchRandomData { imageProperties in
-//            print(imageProperties)
-//        }
-        imageDataManager.fetchRandomImageData { imageData in
-             print("data:", imageData)
+        let swipeUp = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(gesture:)))
+        let swipeDown = UISwipeGestureRecognizer(target: self, action: #selector(respondToSwipeGesture(gesture:)))
+        swipeUp.direction = UISwipeGestureRecognizer.Direction.up
+        swipeDown.direction = UISwipeGestureRecognizer.Direction.down
+        
+        swipeUp.delegate = self
+        swipeDown.delegate = self
+     
+        view.addGestureRecognizer(swipeUp)
+        view.addGestureRecognizer(swipeDown)
+        
+        //generateImageURL()
+        //generateImageURL()
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        for _ in 1...3 {
+        generateImageURL()
         }
     }
     
+
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        1
+//        cvCounter
+        imageLinks.count
     }
     
+
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        contentOffset = collectionView.contentOffset
+       
+        
+//        print(directionIsDown)
+//        print("currentOffset:", currentOffset.y, "contentSize:", contentOffset.y)
+//        if currentOffset.y > (contentOffset.y - 200) && !directionIsDown {
+//            print("generating...")
+//            generateImage()
+//        }
+        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ImageCell", for: indexPath)
-        imageDataManager.fetchRandomImageData { imageData in
-            let image = UIImage(data: imageData)
+        imageDataManager.downloadImageData(url: imageLinks[indexPath.item]) { imageData in
+            guard let image = UIImage(data: imageData) else { return }
             DispatchQueue.main.async {
                 let imageView = UIImageView(image: image)
-                cell.contentView.addSubview(imageView)
+                let frame = CGRect(x: 0, y: 0, width: cell.contentView.frame.width, height: cell.contentView.frame.height)
+                imageView.frame = frame
+                cell.addSubview(imageView)
+//                if self.imageLinks.count < self.maxCount{
+//                    self.generateImageURL()
+//                }
+                
             }
-    
+            print(self.imageLinks.count)
         }
-    
+        
+        
+        
+//        let image = images[indexPath.item]
+//        let imageView = UIImageView(image: image)
+//        let frame = CGRect(x: 0, y: 0, width: cell.contentView.frame.width, height: cell.contentView.frame.height)
+//        imageView.frame = frame
+        
+//        imageDataManager.fetchRandomImageData { imageData in
+//            DispatchQueue.main.async { [self] in
+//            guard let image = UIImage(data: imageData) else{ return }
+//            let imageView = UIImageView(image: image)
+//            cell.contentView.backgroundColor = .blue
+//            cell.contentView.addSubview(imageView)
+//                cvCounter += 1
+//            }
+//        }
+        
+        
+        
         return cell
     }
-
+    
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(width: UIScreen.main.bounds.width / 3 , height: UIScreen.main.bounds.width / 3 )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        UIEdgeInsets(top: 10, left: 0, bottom: 10, right: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        0
+    }
+    
+    override func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        currentOffset = CGPoint(x: scrollView.contentOffset.x, y: scrollView.contentOffset.y)
+        contentOffset.y = scrollView.contentSize.height - scrollView.bounds.height
+//        print("currentOffset:", currentOffset.y, "contentSize:", contentOffset.y)
+    }
+    
+    
+    
+    private func generateImageURL() {
+        
+        imageDataManager.fetchRandomImageURL { [unowned self] url in
+           // guard let image = UIImage(data: imageData) else { return }
+            imageLinks.append(url)
+//            DispatchQueue.main.async {
+//                collectionView.reloadData()
+//            }
+            
+        }
+        
+        collectionView.reloadData()
+    }
+    
     
 }
+
+@available(iOS 13.0, *)
+extension ImageListViewController: UIGestureRecognizerDelegate {
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+            return true
+        }
+    
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+            return true
+        }
+    
+    @objc func respondToSwipeGesture(gesture: UIGestureRecognizer) {
+           if let swipeGesture = gesture as? UISwipeGestureRecognizer {
+               switch swipeGesture.direction {
+               case UISwipeGestureRecognizer.Direction.down:
+                   print("swipeDown")
+                   directionIsDown = true
+               case UISwipeGestureRecognizer.Direction.up:
+                   directionIsDown = false
+                   print("swipeUp")
+               default:
+                   break
+               }
+           }
+       }
+}
+
+
+
 
